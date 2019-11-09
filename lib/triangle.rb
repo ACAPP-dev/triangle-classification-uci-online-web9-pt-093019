0bcb4eddd87aa1dde1319e3e365c29155521d962
Andrew Capp <acapp909@gmail.com>
2019-11-09 21:51:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,10 +12,11 @@ class Triangle
     binding.pry
     if side_a + side_b >= side_c || side_b + side_c >= side_a || side_a + side_c >= side_b
 
-      if side_a == side_b || side_b == side_c || side_a == side_c
-        :isosceles
-      elsif side_a == side_b && side_b == side_c && side_a == side_c
+      if side_a == side_b && side_b == side_c && side_a == side_c
         :equilateral
+      elsif side_a == side_b || side_b == side_c || side_a == side_c
+        :isosceles
+
         #elsif side_a != sid
       #else
       end
