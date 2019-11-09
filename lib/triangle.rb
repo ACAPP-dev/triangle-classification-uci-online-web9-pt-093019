3331581bbf547054393036be1d6da160338c1fde
Andrew Capp <acapp909@gmail.com>
2019-11-09 21:57:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,9 @@ class Triangle
   def kind
     #binding.pry
     if side_a <= 0 || side_b <= 0 || side_c <=0
-      #=error
+      raise TriangleError
     elsif side_a + side_b <= side_c || side_b + side_c <= side_a || side_a + side_c <= side_b
-      #=>error
+      raise TriangleError
     else
       if side_a == side_b && side_b == side_c && side_a == side_c
         :equilateral
